3b9f5bbfa5bf85b0bc8bb89599a403a76deaac7f
Gandionco <gerard-euan.gandionco.767@my.csun.edu>
2024-05-10 20:13:24 -0700

    Implement logging out

--- weather-client/src/app/auth/auth.service.ts
+++ weather-client/src/app/auth/auth.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
-import { Observable, tap } from 'rxjs';
+import { BehaviorSubject, Observable, tap } from 'rxjs';
 import { LoginRequest } from './login-request';
 import { LoginResult } from './login-result';
 import { environment } from '../../environments/environment.development';
@@ -12,7 +12,24 @@ import { environment } from '../../environments/environment.development';
 export class AuthService {
   public tokenKey: string = 'tokenKey';
 
+  private _authStatus = new BehaviorSubject<boolean>(false);
+  public authStatus = this._authStatus.asObservable();
+
   constructor(protected http: HttpClient) { }
+
+  public init(): void {
+    if (this.isAuthenticated()) {
+      this.setAuthStatus(true);
+    }
+  }
+
+  private setAuthStatus(isAuthenticated: boolean): void {
+    this._authStatus.next(isAuthenticated);
+  }
+
+  public isAuthenticated(): boolean {
+    return this.getToken() !== null;
+  }
   
   public login(loginRequest: LoginRequest): Observable<LoginResult> {
     let url = `${environment.baseUrl}/api/admin/login`;
@@ -20,11 +37,17 @@ export class AuthService {
       .pipe(tap(loginResult => {
         if (loginResult.success) {
           localStorage.setItem(this.tokenKey, loginResult.token);
+          this.setAuthStatus(true);
         }
       }));
   }
 
   public getToken(): string | null {
     return localStorage.getItem(this.tokenKey);
+  }
+
+  public logout(): void {
+    localStorage.removeItem(this.tokenKey);
+    this.setAuthStatus(false);
   }
 }
--- weather-client/src/app/navbar/navbar.component.ts
+++ weather-client/src/app/navbar/navbar.component.ts
@@ -1,8 +1,10 @@
-import { Component } from '@angular/core';
-import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Router, RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import {MatIconModule} from "@angular/material/icon";
 import {MatButtonModule} from "@angular/material/button";
 import {MatToolbarModule} from "@angular/material/toolbar";
+import { Subject, takeUntil } from 'rxjs';
+import { AuthService } from '../auth/auth.service';
 
 @Component({
   selector: 'app-navbar',
@@ -11,6 +13,28 @@ import {MatToolbarModule} from "@angular/material/toolbar";
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit, OnDestroy {
+  public isLoggedIn = false;
+  private _destroySubject = new Subject();
 
+  public constructor(private authService: AuthService, private router: Router) {
+    this.authService.authStatus.pipe(takeUntil(this._destroySubject))
+      .subscribe(result => {
+        this.isLoggedIn = result;
+      })
+  }
+
+  public ngOnInit(): void {
+    this.isLoggedIn = this.authService.isAuthenticated();
+  }
+
+  public ngOnDestroy(): void {
+    this._destroySubject.next(true);
+    this._destroySubject.complete();
+  }
+
+  public onLogout(): void {
+    this.authService.logout();
+    this.router.navigate(['login'])
+  }
 }
